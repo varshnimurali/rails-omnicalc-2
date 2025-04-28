08cd71de6c4ad12f36070f9f360d1eac9e346bde
varshnimurali <vmurali0@chicagobooth.edu>
2025-04-28 04:14:04 +0000

    save changes 2

---
+++ app/controllers/subtract_controller.rb
@@ -0,0 +1,14 @@
+class SubtractController < ApplicationController
+  def show_sub_form
+    render({ :template => "subtraction_templates/subtraction_form" })
+  end
+
+  def subtract
+    @first_number = params.fetch("first_num").to_f
+    @second_number = params.fetch("second_num").to_f
+
+    @result =  @second_number - @first_number
+
+    render({ :template => "subtraction_templates/sub_results" })
+  end
+end
